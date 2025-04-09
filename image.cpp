bc37897ed5cbc2d2b729311db22fee9f554ecec5
JoonHo Son <joonho.son@me.com>
2025-04-10 03:00:01 +0900

    이미지 복수 출력 테스트 중
    윈도우에선 우측(IMG_Load 함수) 이미지 출력 에러 발생함.
    맥에서 확인 필요.

--- image.cpp
+++ image.cpp
@@ -6,35 +6,63 @@
 int main() {
 	SDL_Window* window;
 	SDL_Renderer* renderer;
+	constexpr int width = 640;
+	constexpr int height = 480;
+	constexpr int imageWidth = 200;
+	constexpr int imageHeight = 200;
 
-	if (!SDL_CreateWindowAndRenderer("SDL3 Image test", 640, 480, SDL_WINDOW_RESIZABLE, &window, &renderer)) {
+	if (!SDL_CreateWindowAndRenderer("SDL3 Image test", width, height, SDL_WINDOW_RESIZABLE, &window, &renderer)) {
 		std::cout << "SDL_CreateWindowAndRenderer error => " << SDL_GetError() << std::endl;
 
 		return 1;
 	}
 
 	// BMP image
-	SDL_Surface* helloImage = SDL_LoadBMP("assets/kagura.bmp");
+	SDL_Surface* leftImage = SDL_LoadBMP("assets/kagura.bmp");
 
-
-	// JPEG image
-	//SDL_Surface* helloImage = IMG_Load("gintoki.jpg");
-
-	if (helloImage == 0) {
+	if (leftImage == nullptr) {
+		SDL_DestroySurface(leftImage); // 메모리 해제
 		SDL_DestroyRenderer(renderer);
 		SDL_DestroyWindow(window);
 
-		std::cout << "SDL_LoadBMP error => " << SDL_GetError() << std::endl;
+		std::cout << "Left image load fail [assets/kagura.bmp] => " << SDL_GetError() << std::endl;
 
 		return 1;
 	}
 
-	SDL_Texture* texture = SDL_CreateTextureFromSurface(renderer, helloImage);
+	SDL_Texture* leftTexture = SDL_CreateTextureFromSurface(renderer, leftImage);
+	SDL_FRect leftTarget = { static_cast<float>(width) / 4 - static_cast<float>(imageWidth) / 2, static_cast<float>(height) / 2 - static_cast<float>(imageHeight) / 2 , static_cast<float>(imageWidth), static_cast<float>(imageHeight)};
 
-	//SDL_FreeSurface(helloBMP); // 메모리 해제
-	SDL_DestroySurface(helloImage); // 메모리 해제
+	SDL_DestroySurface(leftImage); // 메모리 해제
 
-	if (texture == 0) {
+	if (leftTexture == nullptr) {
+		SDL_DestroyRenderer(renderer);
+		SDL_DestroyWindow(window);
+
+		std::cout << "SDL_CreateTextureFromSurface error => " << SDL_GetError() << std::endl;
+
+		return 1;
+	}
+
+	// JPG image
+	SDL_Surface* rightImage = IMG_Load("assets/gintama.jpg");
+
+	if (rightImage == nullptr) {
+		SDL_DestroySurface(rightImage);
+		SDL_DestroyRenderer(renderer);
+		SDL_DestroyWindow(window);
+
+		std::cout << "Right image load fail [assets/gintama.jpg] => " << SDL_GetError() << std::endl;
+
+		return 1;
+	}
+
+	SDL_Texture* rightTexture = SDL_CreateTextureFromSurface(renderer, rightImage);
+	SDL_FRect rightTarget = { static_cast<float>(width) / 4 * 3 - static_cast<float>(imageWidth) / 2, static_cast<float>(height / 2) - static_cast<float>(imageHeight) / 2, static_cast<float>(imageWidth), static_cast<float>(imageHeight) };
+
+	SDL_DestroySurface(rightImage); // 메모리 해제
+
+	if (rightTexture == nullptr) {
 		SDL_DestroyRenderer(renderer);
 		SDL_DestroyWindow(window);
 
@@ -59,11 +87,13 @@ int main() {
 
 		SDL_RenderClear(renderer);
 		//SDL_RenderCopy(renderer, texture, nullptr, nullptr);
-		SDL_RenderTexture(renderer, texture, nullptr, nullptr);
+		//SDL_RenderTexture(renderer, leftTexture, nullptr, nullptr);
+		SDL_RenderTexture(renderer, leftTexture, nullptr, &leftTarget);
+		SDL_RenderTexture(renderer, rightTexture, nullptr, &rightTarget);
 		SDL_RenderPresent(renderer);
 	}
 
-	SDL_DestroyTexture(texture);
+	SDL_DestroyTexture(leftTexture);
 	SDL_DestroyRenderer(renderer);
 	SDL_DestroyWindow(window);
 	SDL_Quit();
