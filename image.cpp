c41c04bc58575ebc58e149f25840e36384c99799
JoonHo Son <joonho.son@me.com>
2025-04-09 23:32:32 +0900

    assets 이동 처리 중.
    잘 안됨 😑

---
+++ image.cpp
@@ -0,0 +1,72 @@
+#include <iostream>
+#include <SDL3/SDL.h>
+#include <SDL3/SDL_render.h>
+#include <SDL3_image/SDL_image.h>
+
+int main() {
+	SDL_Window* window;
+	SDL_Renderer* renderer;
+
+	if (!SDL_CreateWindowAndRenderer("SDL3 Image test", 640, 480, SDL_WINDOW_RESIZABLE, &window, &renderer)) {
+		std::cout << "SDL_CreateWindowAndRenderer error => " << SDL_GetError() << std::endl;
+
+		return 1;
+	}
+
+	// BMP image
+	SDL_Surface* helloImage = SDL_LoadBMP("kagura.bmp");
+	 
+	 
+	// JPEG image
+	//SDL_Surface* helloImage = IMG_Load("gintoki.jpg");
+
+	if (helloImage == 0) {
+		SDL_DestroyRenderer(renderer);
+		SDL_DestroyWindow(window);
+
+		std::cout << "SDL_LoadBMP error => " << SDL_GetError() << std::endl;
+
+		return 1;
+	}
+
+	SDL_Texture* texture = SDL_CreateTextureFromSurface(renderer, helloImage);
+
+	//SDL_FreeSurface(helloBMP); // 해제
+	SDL_DestroySurface(helloImage); // 해제
+
+	if (texture == 0) {
+		SDL_DestroyRenderer(renderer);
+		SDL_DestroyWindow(window);
+
+		std::cout << "SDL_CreateTextureFromSurface error => " << SDL_GetError() << std::endl;
+
+		return 1;
+	}
+
+	bool running = true;
+
+	while (running) {
+		SDL_Event event;
+
+		while (SDL_PollEvent(&event)) {
+			if (event.type == SDL_EVENT_KEY_DOWN && event.key.key == SDLK_ESCAPE) {
+				running = false;
+			}
+			else if (event.type == SDL_EVENT_QUIT) {
+				running = false;
+			}
+		}
+
+		SDL_RenderClear(renderer);
+		//SDL_RenderCopy(renderer, texture, nullptr, nullptr);
+		SDL_RenderTexture(renderer, texture, nullptr, nullptr);
+		SDL_RenderPresent(renderer);
+	}
+
+	SDL_DestroyTexture(texture);
+	SDL_DestroyRenderer(renderer);
+	SDL_DestroyWindow(window);
+	SDL_Quit();
+
+	return 0;
+}
